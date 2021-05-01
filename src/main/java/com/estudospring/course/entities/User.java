6dd75e8edffc3ee6874cd56b67e4f0dba2811ef8
johnyguido <johnyguido@hotmail.com.br>
2021-05-01 14:33:06 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/estudospring/course/entities/User.java
+++ src/main/java/com/estudospring/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.estudospring.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +26,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -99,6 +105,10 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 }
