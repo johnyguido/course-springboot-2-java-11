cbe5844329f45e0494b9ec8bf0919adef18a89fb
johnyguido <johnyguido@hotmail.com.br>
2021-05-02 14:39:24 -0300

    Order, Instant, ISO 8601 - Complementary update

--- src/main/java/com/estudospring/course/entities/Order.java
+++ src/main/java/com/estudospring/course/entities/Order.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -23,6 +25,7 @@ public class Order implements Serializable {
 
 	private Instant moment;
 
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
